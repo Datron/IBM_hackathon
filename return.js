e4539c076943947d9ad2a8ae38051a6f8bf9ecef
indranil1741997 <indranil1741997@gmail.com>
2016-10-01 15:03:17 +0530

    updated code

--- return.js
+++ return.js
@@ -106,17 +106,17 @@ else if(msg.payload.top_class =="Singers")
     }
 else if(msg.payload.top_class =="Actresses")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top Bollywood Actresses " +
+                        "1.  Deepika Padukone " +
+                        "2.  Priyanka Chopra " +
+                        "3.  Kareena Kapoor";
     }
 else if(msg.payload.top_class =="Actors")
     {
-         msg.payload ="@"+msg.sender + "Top Actors of All Time " +
-                        "1. Jack Nicholson " +
-                        "2. Marlon Brando" +
-                        "3. Robert De Niro ";
+         msg.payload ="@"+msg.sender + "Top Bollywood Actors " +
+                        "1. Shah Rukh Khan " +
+                        "2. Amitabh Bachchan" +
+                        "3. Salman Khan ";
     }
 else if(msg.payload.top_class =="MusicDirectors")
     {
@@ -135,9 +135,9 @@ else if(msg.payload.top_class =="GuitarArtists")
 else if(msg.payload.top_class =="TablaArtists")
     {
          msg.payload ="@"+msg.sender + "Top tabla Artists of All Time " +
-                        "1. scd   " +
-                        "2.  dcs " +
-                        "3. dcs ";
+                        "1. Zakir Hussain  " +
+                        "2. Alla Rakha " +
+                        "3. Samta Prasad ";
     }
 else if(msg.payload.top_class =="Soaps")
     {
@@ -169,121 +169,135 @@ else if(msg.payload.top_class =="PartySongs")
     }
 else if(msg.payload.top_class =="BestBooks")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Best Books of all Time " +
+                        "1.  To kill A Mockingbird-Harper Lee " +
+                        "2.  1984-George Orwell " +
+                        "3.  The Lord Of The Rings-J.R.R Tolkien";
     }
 else if(msg.payload.top_class =="WorstBooks")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Worst Books Of All Time " +
+                        "1.  Twilight(#1)-Stephenie Meyer " +
+                        "2.  Breaking Dawn-Stephenie Mayer" +
+                        "3.  New Moon-Stephenie Mayer " +
     }
 else if(msg.payload.top_class =="FictionBooks")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top Fiction Books " +
+                        "1.  The God of Small Things " +
+                        "2.  The White Tiger " +
+                        "3.  Midnight's Children";
     }
 else if(msg.payload.top_class =="Biographies")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Best Biographies of all Time" +
+                        "1.  Unbroken-Laura Hillenbrand " +
+                        "2.  The Autobiography of Malcolm X" +
+                        "3.  The diary of a Young Girl-Anne Frank";
     }
 else if(msg.payload.top_class =="Authors")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Best authors of All Time " +
+                        "1.  William Shakespeare " +
+                        "2.  Leo Tolstoy " +
+                        "3.  Charles Dickens";
     }
 else if(msg.payload.top_class =="BeautifulWomen")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1.  Daly College,Indore " +
-                        "2.  Delhi Public School, Delhi " +
-                        "3.  Modern School, Delhi";
+         msg.payload ="@"+msg.sender + " Beautiful Women " +
+                        "1.  Selena Gomez (USA) " +
+                        "2.  Liza Soberano (Philippines) " +
+                        "3.  Deepika Padukone (India)";
     }
 else if(msg.payload.top_class =="HottestWomen")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Hottest Women " +
+                        "1. Shailene Woodley " +
+                        "2. Jennifer Lopez " +
+                        "3. Deepika Padukone";
     }
 else if(msg.payload.top_class =="HottestMen")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top hottest men " +
+                        "1. Ranveer Singh " +
+                        "2. Hrithik Roshan " +
+                        "3. Shah Rukh Khan";
     }
 else if(msg.payload.top_class =="SexiestMen")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top sexiest men " +
+                        "1.  Jensen Ackles" +
+                        "2.  Jared Padalecki " +
+                        "3. Johnny Depp ";
     }
 else if(msg.payload.top_class =="Comedians")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top comedians " +
+                        "1. George Carlin" +
+                        "2. Louis C.K." +
+                        "3. Dave Chappelle ";
     }
 else if(msg.payload.top_class =="HorrorMovies")
     {
-         msg.payload ="@"+msg.sender+"Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender+"Top horror movies " +
+                        "1. The Exorcist " +
+                        "2. The Shining " +
+                        "3. The Conjouring ";
     }
-else if(msg.payload.top_class =="Fashion")
+else if(msg.payload.top_class =="Fashion Brands")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top fashion brands" +
+                        "1. LOUIS VUITTON" +
+                        "2. HERMES " +
+                        "3. GUCCI";
     }
 else if(msg.payload.top_class =="FashionsDesigners")
     {
-         msg.payload ="@"+msg.sender+"Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender+"Top Fashion designers" +
+                        "1. Manish Malhotra " +
+                        "2. Sabyasachi Mukherji" +
+                        "3. Tarun Tahiliani ";
     }
 else if(msg.payload.top_class =="FashionColleges")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top fashion colleges " +
+                        "1. National Institute of Fashion Technology (NIFT) Delhi " +
+                        "2. Pearl Academy" +
+                        "3. National Institute of Fashion Technology (NIFT) Mumbai";
     }
 else if(msg.payload.top_class =="Shoes")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top shoes brands " +
+                        "1. Nike  " +
+                        "2. Addidas " +
+                        "3. Reebok";
     }
 else if(msg.payload.top_class =="deodrants")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top deodrants brands " +
+                        "1. Axe  " +
+                        "2. Dove" +
+                        "3. L’OCCITANE";
     }
 else if(msg.payload.top_class =="Jewellary")
     {
-         msg.payload ="@"+msg.sender + "Top Private Schools " +
-                        "1. Daly College,Indore " +
-                        "2. Delhi Public School, Delhi " +
-                        "3. Modern School, Delhi";
+         msg.payload ="@"+msg.sender + "Top jewellary brands " +
+                        "1. Tanishq " +
+                        "2. Nakshatra Diamond Jewellery  " +
+                        "3. D'Damas Jewellery ";
+    }
+else if(msg.payload.top_class =="Restaurants")
+    {
+         msg.payload ="@"+msg.sender + "Top Restaurants in Bangalore " +
+                        "1. Karavalli " +
+                        "2. High Ultra Lounge  " +
+                        "3. The Ritz-Carlton ";
+    }
+else if(msg.payload.top_class =="Programmers")
+    {
+         msg.payload ="@"+msg.sender + "My Parents " +
+                        "1. Kartik " +
+                        "2. Indranil  " +
+                        "3. Vrinda "+"They Created Me";
     }
     return msg;
